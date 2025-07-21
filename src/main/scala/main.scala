455ec8203068f7ea766d9e392f83e945a2d2a1cb
arthurMedforth <arthur.medforth@accenture.com>
2025-07-21 11:25:55 +0100

    comment

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -11,5 +11,5 @@ def main(): Unit =
   for (i <- 1 to 5) do
     //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
     // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    println(s"i = $i")
+    println(s"i = $i") // println same as in groovy
 
